d26f937d084fab1ff0e15cd11ac86f9a9fd31bf0
king0120 <king0120@gmail.com>
2017-08-08 08:45:37 -0400

    jeopardy

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Game from './components/Game';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Game from "./components/Game";
+import AddCategory from "./components/AddCategory";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <Game />
-      </div>
+      <Router>
+        <div>
+          <div>
+            <Link to="/">Home</Link>
+            <Link to="/add-category">Add Category</Link>
+          </div>
+          <div className="App">
+            <Route exact path="/" component={Game} />
+            <Route path="/add-category" component={AddCategory} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
