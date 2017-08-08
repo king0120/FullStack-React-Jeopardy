445cb0ea3eb03654fe6cf7f873d59a61c6ed4bcf
king0120 <king0120@gmail.com>
2017-08-08 18:20:30 -0400

    pushing

--- controllers/game.js
+++ controllers/game.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const Question = require('../models/Question');
-const Category = require('../models/Category');
+const Question = require('../models/question');
+const Category = require('../models/category');
 const Game = require('../models/game');
 const router = express.Router();
 
